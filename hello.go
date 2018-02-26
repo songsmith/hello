80999636a1e54260ec849e599a0cefd147697a61
songsmith <songshaodong@kingsoft.com>
2018-02-26 11:28:08 +0800

    modify format

--- hello.go
+++ hello.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-        "fmt"
-        "github.com/songsmith/stringutil"
+	"fmt"
+	"github.com/songsmith/stringutil"
 )
+
 func main() {
-        fmt.Printf("Hello, world.\n")
-        fmt.Printf(stringutil.Reverse("\n !oG , olleH"))
+	fmt.Printf("Hello, world.\n")
+	fmt.Printf(stringutil.Reverse("\n !oG , olleH"))
 }
